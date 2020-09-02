797a20ab2f729acc8f677f8ef2e12a7f2b8b8a48
Emiliano Castagnari (a.k.a Torian) <ecastag@gmail.com>
2020-09-01 23:09:08 -0300

    Update for AWS provider

--- acm.tf
+++ acm.tf
@@ -13,20 +13,24 @@ resource "aws_acm_certificate" "crt" {
 }
 
 resource "aws_route53_record" "crt-validation" {
-  name = aws_acm_certificate.crt.domain_validation_options.0.resource_record_name
-  type = aws_acm_certificate.crt.domain_validation_options.0.resource_record_type
-
-  records = [
-    aws_acm_certificate.crt.domain_validation_options.0.resource_record_value
-  ]
+  for_each = {
+    for dvo in aws_acm_certificate.crt.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
   zone_id = var.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [ each.value.record ]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "crt-validation" {
   certificate_arn         = aws_acm_certificate.crt.arn
-  validation_record_fqdns = [ aws_route53_record.crt-validation.fqdn ]
+  validation_record_fqdns = [ for record in aws_route53_record.crt-validation : record.fqdn ]
 }
 
 output "crt" {
